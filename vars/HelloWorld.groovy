92e44a4c77146be95969a7edb8ae8a0ac20c7e01
gouthz <39449032+gouthz@users.noreply.github.com>
2021-04-01 16:55:38 +0530

    Update HelloWorld.groovy

--- vars/HelloWorld.groovy
+++ vars/HelloWorld.groovy
@@ -1,9 +1,27 @@
-import org.dcube.build.HelloWorld
-import org.dcube.ioc.ContextRegistry
+def call(Closure body) {
+    // evaluate the body block, and collect configuration into the object
+    def params = [:]
 
-def call(String solutionPath) {
-    ContextRegistry.registerDefaultContext(this)
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = params
+    body()
 
-    def helloworld = new HelloWorld(solutionPath)
-    helloworld.build()
-}
+    pipeline {
+        agent any
+        options { skipDefaultCheckout()
+                  buildDiscarder(logRotator(numToKeepStr:'25'))
+        }
+        stages {
+            // Add jenkins environment variables to configuration
+            stage('Env') {
+                steps {
+                    script {
+                        checkout scm
+                        getEnvVariables(params)
+                        TAG = "phoenix-development-${params.BUILD_NUMBER}"
+
+                    }
+                }
+            }
+        }
+    }
